f4319fcce3bc3575df9dab2d384590f7fbe265d3
y2gcoder <y2gcoder@gmail.com>
2022-10-06 20:22:28 +0900

    Initial Commit

---
+++ src/test/java/com/y2gcoder/app/SpringBootApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2gcoder.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
