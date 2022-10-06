460c54a78042552082c562411a7c2b7f12258d99
y2gcoder <y2gcoder@gmail.com>
2022-10-06 20:29:19 +0900

    JPA Auditing 추가

---
+++ src/main/java/com/y2gcoder/app/global/config/jpa/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.y2gcoder.app.global.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
