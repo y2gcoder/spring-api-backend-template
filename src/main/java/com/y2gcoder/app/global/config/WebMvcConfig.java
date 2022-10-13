6e0b27003aacd20ec2e40af1fa77ec28d60e4c4f
y2gcoder <y2gcoder@gmail.com>
2022-10-13 15:42:11 +0900

    cors 에러 해결

--- src/main/java/com/y2gcoder/app/global/config/WebMvcConfig.java
+++ src/main/java/com/y2gcoder/app/global/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("*")  //TODO
+				.allowedOriginPatterns("*")
 				.allowedMethods(
 						HttpMethod.GET.name(),
 						HttpMethod.POST.name(),
