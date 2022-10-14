3a66cee31c5689000f00b2f64ebfb7295f215771
y2gcoder <y2gcoder@gmail.com>
2022-10-14 21:02:19 +0900

    XSS 방어 추가

--- src/main/java/com/y2gcoder/app/global/config/WebMvcConfig.java
+++ src/main/java/com/y2gcoder/app/global/config/WebMvcConfig.java
@@ -1,12 +1,26 @@
 package com.y2gcoder.app.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import com.y2gcoder.app.global.config.web.HtmlCharacterEscapes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final ObjectMapper objectMapper;
+
 	private static final long MAX_AGE_SECS = 3600;
 
 	@Override
@@ -24,5 +38,26 @@ public class WebMvcConfig implements WebMvcConfigurer {
 				.allowedHeaders("*")
 				.allowCredentials(true)
 				.maxAge(MAX_AGE_SECS);
+	}
+
+	@Bean
+	public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+		FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new XssEscapeServletFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(jsonEscapeConverter());
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+		ObjectMapper copy = objectMapper.copy();
+		copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+		return new MappingJackson2HttpMessageConverter(copy);
 	}
 }
